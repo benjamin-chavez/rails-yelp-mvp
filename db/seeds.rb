82790f7efb9362198435e6bed579fe1ca8188f21
Ben Chavez <ben.m.chavez@gmail.com>
2020-02-19 19:21:19 +0100

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# db/seeds.rb
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category:     'chinese',
+    phone_number: '01 43 54 23 31'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category:     "italian",
+    phone_number: '01 43 54 23 31'
+  },
+  {
+    name:         'MCD',
+    address:      'checkpoint charlie',
+    category:     "french",
+    phone_number: '01 43 54 23 31'
+  },
+  {
+    name:         'Jimanos',
+    address:      'gurnee',
+    category:     "italian",
+    phone_number: '01 43 54 23 31'
+  },
+  {
+    name:         'Peaquads',
+    address:      'milwaukee ave',
+    category:     "italian",
+    phone_number: '01 43 54 23 31'
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
